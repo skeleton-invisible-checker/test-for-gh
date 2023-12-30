26a40ba212bd069ec8480e77615907786f9caece
Takuo Kaneko <alwaysrunner0720@gmail.com>
2023-12-30 15:00:15 +0900

    update index.css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Base from './Base'
 function App() {
   return (
       <div className="App">
-        <h1>Skeleton Invisible Checker</h1>
+          <h1>Skeleton Invisible Checker</h1>
       </div>
   );
 }
